1bbc4d41878fd4bdd93b7799019fd32f9a240719
MaruMaruyama <maruyama@zukat.jp>
2020-10-22 21:06:52 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+   int Add(int a, int b)
+   {
+       int ret;
+       ret=a+b;
+       return ret;
+   } 
+    // Start is called before the first frame update
+    void Start()
+    {
+        int answer;
+        answer= Add(5,7);
+        Debug.Log(answer);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
