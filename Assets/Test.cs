e89cc7d6d499e6dfbf263a3fb6bbea1dcbd2de90
MaruMaruyama <maruyama@zukat.jp>
2020-10-26 14:13:04 +0900

    課題の提出
    課題と発展課題について記載しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-   public class Boss{
+   public class Boss
+   {
        private int hp = 100; //体力
        private int power = 25;  // 攻撃力
+       private int mp = 53; //MPの初期化と値の代入
 
        //攻撃用の関数
        public void attack()
@@ -18,20 +20,38 @@ using UnityEngine;
            Debug.Log( damage+"のダメージを受けた");
            this.hp -= damage;
        }
+        //発展課題で使用するMagic関数
+       public void Magic(int turn)
+       {
+           for(int l = 0; l < turn ; l++){
+               if(mp >= 5){
+                   mp -= 5;
+                   Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
+               }else{
+                   Debug.Log("MPが足りない！");
+               }  
+            }
+        }
    }
-
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Boss lastboss = new Boss();
-        lastboss.attack();
-        lastboss.defence(3);
-
-        Boss midboss = new Boss();
-        midboss.attack();
-        midboss.defence(20);
+        //課題1　増加
+        int[] array = {20,30,50,90,40};
+        for (int i = 0; i < array.Length; i++){
+            Debug.Log( array[i]);
+        }
+        //課題1　減少
+        for (int i = array.Length - 1; i >= 0; i--){
+            Debug.Log(array[i]);
+        }
+        //発展課題用 testbossインスタンスにボスクラスを入れています
+        Boss testboss = new Boss();
+        //ターン数を引数に入れています。戻り値はなし。
+            testboss.Magic(10);
+            testboss.Magic(1);
     }
     // Update is called once per frame
     void Update()
