74812e1b434d249e978af074d5a70a1d260012b0
MaruMaruyama <maruyama@zukat.jp>
2020-10-26 13:12:18 +0900

    Update Test.cs
    bossクラスの学習

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,20 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+   public class Boss{
+       private int hp = 100; //体力
+       private int power = 25;  // 攻撃力
+
+       //攻撃用の関数
+       public void attack()
+       {
+           Debug.Log(this.power + "のダメージを与えた");
+       }
+
+       //防御用の関数
+       public void defence(int damage)
+       {
+           Debug.Log( damage+"のダメージを受けた");
+           this.hp -= damage;
+       }
+   }
+
 public class Test : MonoBehaviour
 {
-   int Add(int a, int b, int c)
-   {
-       int d = a + b + c;
-       return d;
-   } 
     // Start is called before the first frame update
     void Start()
     {
-        //戻り値cをnum にint 型として代入
-        int num = Add(3,6,5);
-        Debug.Log(num);
-        　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　
+        Boss lastboss = new Boss();
+        lastboss.attack();
+        lastboss.defence(3);
+
+        Boss midboss = new Boss();
+        midboss.attack();
+        midboss.defence(20);
     }
     // Update is called once per frame
     void Update()
